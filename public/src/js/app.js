e71d87dbe0f6e1076cbddb3c0d47c09daa7bcbd8
Robert Lowe <robertmlowe@rmlowe.com>
2020-10-03 12:52:13 +0800

    Async Code in JavaScript

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -2,16 +2,22 @@
 var deferredPrompt;
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('/sw.js')
-        .then(function() {
-            console.log('Service worker registered!');
-        });
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    });
 }
 
-window.addEventListener('beforeinstallprompt', function(event) {
-    console.log('beforeinstallprompt fired');
-    event.preventDefault();
-    deferredPrompt = event;
-    return false;
+window.addEventListener('beforeinstallprompt', function (event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
 });
+
+setTimeout(function () {
+  console.log('This is executed once the timer is done!');
+}, 3000);
+
+console.log('This is executed right after setTimeout()');
