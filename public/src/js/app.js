27a2b7eff9986863d7a572cee9a5dfad5177d44b
Robert Lowe <robertmlowe@rmlowe.com>
2020-10-03 13:10:57 +0800

    Rejecting Promises

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -18,15 +18,26 @@ window.addEventListener('beforeinstallprompt', function (event) {
 
 var promise = new Promise(function (resolve, reject) {
   setTimeout(function () {
-    resolve('This is executed once the timer is done!');
+    //resolve('This is executed once the timer is done!');
+    reject({ code: 500, message: 'An error occurred!' });
     //console.log('This is executed once the timer is done!');
   }, 3000);
 });
+
+// promise.then(function (text) {
+//   return text;
+// }, function (err) {
+//   console.log(err.code, err.message)
+// }).then(function (newText) {
+//   console.log(newText);
+// });
 
 promise.then(function (text) {
   return text;
 }).then(function (newText) {
   console.log(newText);
+}).catch(function (err) {
+  console.log(err.code, err.message);
 });
 
 console.log('This is executed right after setTimeout()');
